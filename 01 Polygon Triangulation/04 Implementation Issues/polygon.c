854a3b7da0780be41a2e4493df48011a47874c7f
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-01-02 14:10:46 +0000

    start of points & polygons

---
+++ 01 Polygon Triangulation/04 Implementation Issues/polygon.c
@@ -0,0 +1,17 @@
+/************************
+   build with:
+     gcc polygon.c -o polygon
+   run with:
+     ./polygon
+*************************/
+
+#include <stdio.h>
+
+int main() {
+  // learn the build cylce 
+  printf("Hello, Mate!\n\n");
+  printf("Points & Polygons \n");
+
+  return 0;
+}
+
--- main.sh
+++ main.sh
@@ -11,7 +11,7 @@ echo
 # ./path-to-sample/sample
 
 art_gallery=$PWD/"01 Polygon Triangulation/01 Art Gallery Theorems"
-
+implementation=$PWD/"01 Polygon Triangulation/04 Implementation Issues"
 echo "building: "
 echo '+------------------+'
 echo "hello.c"
@@ -19,5 +19,6 @@ echo '+------------------+'
 echo "binaries in ./Results"
 echo '+------------------+'
 
-gcc "$art_gallery/"hello.c -o ./Results/hello
+gcc "$art_gallery/"hello.c -o ./Results/gallery_hello
+gcc "$implementation/"hello.c -o ./Results/implementation_hello
 #./Results/gcd
